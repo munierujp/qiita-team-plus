ca73db61783182671c6bf81af268497df2973f0c
Munieru <20086673+munierujp@users.noreply.github.com>
2019-04-30 20:06:42 +0900

    Add autoEnableSyncScroll action

--- app/scripts/modules/routes.js
+++ app/scripts/modules/routes.js
@@ -1,2 +1,11 @@
+import paths from './paths'
+import actions from './actions'
+
 export default [
+  {
+    path: paths.editArticle,
+    actions: [
+      actions.autoEnableSyncScroll
+    ]
+  }
 ]
