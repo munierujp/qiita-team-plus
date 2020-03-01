bbe169be1badcca4d4c73bb6987322d5cf52981c
Munieru <20086673+munierujp@users.noreply.github.com>
2020-03-02 00:33:16 +0900

    Add getMetadata.js (#41)

--- app/scripts/modules/getCurrentUserId.js
+++ app/scripts/modules/getCurrentUserId.js
@@ -1,11 +1,8 @@
+import getMetadata from './getMetadata'
+
 function getCurrentUserId () {
-  const userId = Array.from(document.querySelectorAll('script'))
-    .map(script => script.textContent)
-    .map(text => text.match(/"url_name":"(.+?)"/))
-    .filter(matched => matched)
-    .map(matched => matched[1])
-    .find(userId => userId)
-  return userId
+  const metadata = getMetadata()
+  return metadata.user.url_name
 }
 
 export default getCurrentUserId
