c83312e7d017888e3ae683911195da9bf81672db
Munieru <20086673+munierujp@users.noreply.github.com>
2020-03-03 22:40:21 +0900

    Remove getMetadata.js

--- app/scripts/modules/getMetadata.js
+++
@@ -1,7 +0,0 @@
-function getMetadata () {
-  const script = document.querySelector('[data-component-name="HeaderContainer"]')
-  const scriptBody = script.textContent
-  return JSON.parse(scriptBody)
-}
-
-export default getMetadata
