8bb317295f1823ecf6fa3b397e5147d6e790f463
Munieru <20086673+munierujp@users.noreply.github.com>
2020-03-04 00:42:06 +0900

    Remove console.log()

--- app/scripts/contentsscript.js
+++ app/scripts/contentsscript.js
@@ -3,14 +3,12 @@ import actions from './actions'
 import fetchConfigOrDefault from './fetchConfigOrDefault'
 
 if (isTeamPage(document)) {
-  console.log('Qiita Team Plusを実行します。')
   Object.values(actions)
     .filter(({ routes }) => routes.some(route => window.location.pathname.match(route)))
     .forEach(action => {
       fetchConfigOrDefault(action.key)
         .then(enabled => {
           if (enabled) {
-            console.log(`アクション「${action.name}」を実行します。`)
             action.run()
           }
         })
