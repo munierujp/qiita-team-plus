751d0e88e5e0d2cbf47f08ce9fabb1b6059e4075
Munieru <20086673+munierujp@users.noreply.github.com>
2020-03-08 00:27:29 +0900

    Update options page

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -2,35 +2,27 @@ import Vue from 'vue'
 import Storage from './Storage'
 import defaultConfig from './defaultConfig'
 import actions from './actions'
+import createObjectFromEntries from './modules/createObjectFromEntries'
 import AppCheckbox from './components/AppCheckbox'
 
 document.addEventListener('DOMContentLoaded', () => {
   Storage.fetch(defaultConfig).then(config => {
-    const toData = ({ key, name }) => ({
-      key,
-      value: config[key],
-      name
-    })
-
     /* eslint-disable no-new */
     new Vue({
       el: '#app',
       data: () => ({
         status: '',
-        actions: {
-          addOldArticleAlert: toData(actions.addOldArticleAlert),
-          addStockButtonToHeader: toData(actions.addStockButtonToHeader),
-          autoEnableSyncScroll: toData(actions.autoEnableSyncScroll),
-          fixHeader: toData(actions.fixHeader)
-        }
+        actions: createObjectFromEntries(Object.values(actions)
+          .map(({ key, value, name }) => [key, {
+            key,
+            value: config[key],
+            name
+          }]))
       }),
       computed: {
         config () {
-          const config = {}
-          Object.values(this.actions).forEach(action => {
-            config[action.key] = action.value
-          })
-          return config
+          return createObjectFromEntries(Object.values(this.actions)
+            .map(({ key, value }) => [key, value]))
         }
       },
       methods: {
