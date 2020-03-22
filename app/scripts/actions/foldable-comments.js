56c9adfcd8b9c8cb26312b1f735b636f0a0f95b8
Munieru <20086673+munierujp@users.noreply.github.com>
2020-03-22 23:16:11 +0900

    remove config

--- app/scripts/actions/foldable-comments.js
+++ app/scripts/actions/foldable-comments.js
@@ -11,7 +11,7 @@ export default {
   routes: [
     paths.article
   ],
-  run: (config) => {
+  run: () => {
     const page = new ArticlePage(document)
     const commentList = page.getCommentListElement()
     const comments = commentList.getCommentElements()
