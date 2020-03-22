f86c7b7d88aeeb0a1ea2a17ac61f164c3cc29d50
Munieru <20086673+munierujp@users.noreply.github.com>
2020-03-22 23:04:40 +0900

    add foldable_comments

--- app/scripts/actions/index.js
+++ app/scripts/actions/index.js
@@ -2,10 +2,12 @@ import addOldArticleAlert from './add-old-article-alert'
 import addStockButtonToHeader from './add-stock-button-to-header'
 import autoEnableSyncScroll from './auto-enable-sync-scroll'
 import fixHeader from './fix-header'
+import foldableComments from './foldable-comments'
 
 export default {
   addOldArticleAlert,
   addStockButtonToHeader,
   autoEnableSyncScroll,
-  fixHeader
+  fixHeader,
+  foldableComments
 }
