3348d04f47625e6d75a020d5001832a521d15b8d
Munieru <20086673+munierujp@users.noreply.github.com>
2020-05-17 02:12:35 +0900

    update modules

--- app/scripts/modules/create-object-from-entries.js
+++ app/scripts/modules/create-object-from-entries.js
@@ -1,9 +1,7 @@
-function createObjectFromEntries (iterable) {
+export default function (iterable) {
   const obj = {}
   iterable.forEach(([key, value]) => {
     obj[key] = value
   })
   return obj
 }
-
-export default createObjectFromEntries
