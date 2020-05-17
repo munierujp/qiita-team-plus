410f750b61657c31f76e726543d82110fa77a79a
Munieru <20086673+munierujp@users.noreply.github.com>
2020-05-17 11:28:33 +0900

    update import/export

--- app/scripts/modules/index.js
+++ app/scripts/modules/index.js
@@ -1,15 +1,6 @@
-import createObjectFromEntries from './create-object-from-entries'
-import fetchConfig from './fetch-config'
-import isTeamPage from './is-team-page'
-import newDate from './new-date'
-import paths from './paths'
-import Storage from './storage'
-
-export {
-  createObjectFromEntries,
-  fetchConfig,
-  isTeamPage,
-  newDate,
-  paths,
-  Storage
-}
+export { default as createObjectFromEntries } from './create-object-from-entries'
+export { default as fetchConfig } from './fetch-config'
+export { default as isTeamPage } from './is-team-page'
+export { default as newDate } from './new-date'
+export { default as paths } from './paths'
+export { default as Storage } from './storage'
